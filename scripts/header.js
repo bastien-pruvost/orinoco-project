43cba1b091533496e2fa8df6c702b3afe8295d6f
Bastien Pruvost <bastien.mory@gmail.com>
2022-02-21 19:29:41 +0100

    Homepage and header finished

--- scripts/header.js
+++ scripts/header.js
@@ -1,34 +1,26 @@
-const toggleNavBtn = document.getElementById("toggle-nav-btn");
-const navbar = document.getElementById("navbar");
-
-const toggleSearchBtn = document.getElementById("toggle-search-btn");
-const searchbar = document.getElementById("searchbar");
-
+// Global function
 main();
-
 function main() {
-
   toggleNav();
   toggleSearch();
-
 }
 
-
 function toggleNav() {
+  const navbar = document.getElementById("navbar");
 
-  toggleNavBtn.addEventListener("click", function (e) {
+  document.getElementById("toggle-nav-btn").addEventListener("click", function (e) {
     e.preventDefault();
     e.stopPropagation();
     navbar.classList.toggle("active");
 
-    if (searchbar.classList.contains("active")) {
-      searchbar.classList.remove("active");
+    if (document.getElementById("searchbar").classList.contains("active")) {
+      document.getElementById("searchbar").classList.remove("active");
     }
-
   });
 
   document.body.addEventListener("click", function (e) {
-    if (navbar.classList.contains("active")) {
+    if (navbar.classList.contains("active") && e.target.offsetParent.id !== "navbar") {
+      e.preventDefault();
       navbar.classList.remove("active");
     }
   });
@@ -39,22 +31,39 @@ function toggleNav() {
     }
   });
 
+  window.addEventListener("scroll", function () {
+    if (navbar.classList.contains("active")) {
+      navbar.classList.remove("active");
+    }
+  });
+
 };
 
 function toggleSearch() {
+  const searchbar = document.getElementById("searchbar");
 
-  toggleSearchBtn.addEventListener("click", function (e) {
+  document.getElementById("toggle-search-btn").addEventListener("click", function (e) {
     e.stopPropagation();
-    searchbar.classList.toggle("active");
 
-    if (navbar.classList.contains("active")) {
-      navbar.classList.remove("active");
+    setTimeout(() => {
+      searchbar.classList.toggle("active");
+    }, 400);
+
+    if (document.getElementById("navbar").classList.contains("active")) {
+      document.getElementById("navbar").classList.remove("active");
     }
 
   });
 
   document.body.addEventListener("click", function (e) {
     if (searchbar.classList.contains("active") && (e.target.offsetParent.id !== "searchbar")) {
+      e.preventDefault();
+      searchbar.classList.remove("active");
+    }
+  });
+
+  window.addEventListener("scroll", function () {
+    if (searchbar.classList.contains("active")) {
       searchbar.classList.remove("active");
     }
   });
