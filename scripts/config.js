8d0467cdb1e237c799007bc09e9aa1d8f001b381
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-17 11:00:23 +0200

    Update API Url

--- scripts/config.js
+++ scripts/config.js
@@ -4,7 +4,7 @@ let apiUrl;
 if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
   apiUrl = 'http://localhost:3000';
 } else {
-  apiUrl = 'http://training-orinoco-backend.pruvostbastien.fr';
+  apiUrl = 'http://orinoco-backend.pruvostbastien.fr';
 }
 
 console.log(apiUrl);
