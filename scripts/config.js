7cf2853b524bd83451f15018c5d14817a6d2cb81
Bastien Pruvost <bastien.mory@gmail.com>
2022-06-17 10:56:26 +0200

    change https to http for API

--- scripts/config.js
+++ scripts/config.js
@@ -4,7 +4,7 @@ let apiUrl;
 if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
   apiUrl = 'http://localhost:3000';
 } else {
-  apiUrl = 'https://training-orinoco-backend.pruvostbastien.fr';
+  apiUrl = 'http://training-orinoco-backend.pruvostbastien.fr';
 }
 
 console.log(apiUrl);
